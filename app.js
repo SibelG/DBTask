2402fba15897178aca3f7f7de50aae6362bf689b
Sibel GENÇTÜRK <46397935+SibelG@users.noreply.github.com>
2023-02-10 22:11:36 +0300

    added

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+import express from "express";
+
+const app=express;
+
+app.length("/", (req,res)=>{
+    res.send("Index Page");
+});
+server.listen(3000, () => {
+ console.log('Uygulama çalıştırıldı...');
+});
